e54849fbe88cd18db612565b0c21bc1a6cfbb8a8
jlbuild <juliabuildbot@gmail.com>
2026-06-02 18:33:44 +0000

    GAP_pkg_guava_jll build 301.210000.0+0

--- src/GAP_pkg_guava_jll.jl
+++ src/GAP_pkg_guava_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("GAP_pkg_guava")
-JLLWrappers.@generate_main_file("GAP_pkg_guava", UUID("70456840-cd51-5d76-a902-d128bd76a730"))
+JLLWrappers.@generate_main_file("GAP_pkg_guava", Base.UUID("70456840-cd51-5d76-a902-d128bd76a730"))
 end  # module GAP_pkg_guava_jll
